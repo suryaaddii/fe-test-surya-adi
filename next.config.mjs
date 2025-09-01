6ae1ca80901ef337c39f8cda737f5eaf733ccae0
Surya Adi <surya.datago@gmail.com>
2025-09-01 10:03:41 +0700

    fix: proxy API via rewrites (/_api) & axios baseURL relative

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,14 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
+        source: "/_api/:path*",
         destination: "https://test-fe.mysellerpintar.com/api/:path*",
       },
       {
-        source: "/upload/:path*",
+        source: "/_upload/:path*",
         destination: "https://test-fe.mysellerpintar.com/upload/:path*",
       },
     ];
   },
 };
-
 export default nextConfig;
